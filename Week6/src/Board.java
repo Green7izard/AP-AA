2238c0957441b6164e9fb35e2bfa957b43edd10e
Bas van Summeren <basvansummeren@hotmail.com>
2015-12-16 10:39:57 +0100

    Made the HillCLimber for the RightDown problem!

---
+++ Week6/src/Board.java
@@ -0,0 +1,144 @@
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by Bas on 16-12-2015.
+ */
+public class Board
+{
+    public static final int DEFAULT_MAX = 10;
+    private final int board[/*X*/][/*Y*/];
+
+    public Board(int newBoard[][])
+    {
+        board = newBoard;
+    }
+
+    public Board(int width, int height, int maxValue)
+    {
+        this(generateBoard(width, height, maxValue));
+    }
+
+    public Board(int size)
+    {
+        this(size, size, DEFAULT_MAX);
+    }
+
+    private static int[][] generateBoard(int width, int height, int maxValue)
+    {
+        int newBoard[][] = new int[width][height];
+        Random random = new Random();
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                newBoard[x][y] = random.nextInt(maxValue) + 1;
+            }
+        }
+        newBoard[0][0] = 0;
+        newBoard[width - 1][height - 1] = 0;
+
+        return newBoard;
+    }
+
+    public int getLengthForSequence(List<RightDown> list)
+    {
+        try
+        {
+            int length = 0;
+            int x = 0;
+            int y = 0;
+            for (RightDown step : list)
+            {
+                if (step != null)
+                {
+                    if (step == RightDown.RIGHT)
+                    {
+                        x++;
+                    } else if (step == RightDown.DOWN)
+                    {
+                        y++;
+                    }
+                    length += getSquare(x, y);
+                }
+            }
+            return length;
+        } catch (Exception e)
+        {
+            return Integer.MAX_VALUE;
+        }
+    }
+
+    public int getSquare(int x, int y)
+    {
+        try
+        {
+            return board[x][y];
+        } catch (Exception e)
+        {
+            return Integer.MAX_VALUE;
+        }
+    }
+
+    public int getHeight()
+    {
+        if (board.length > 0)
+        {
+            return board[0].length;
+        } else
+        {
+            return - 1;
+        }
+    }
+
+    public int getWidth()
+    {
+        return board.length;
+    }
+
+    public int[][] getBoard()
+    {
+        return board;
+    }
+
+    public void printBoard()
+    {
+        int rowCounter = 0;
+        int columnCounter = 0;
+
+        for (int row = 0; row < getHeight(); row++)
+        {
+            for (int i = 0; i < getWidth(); i++)
+            {
+                System.out.print("+ - ");
+            }
+            System.out.println("+");
+            System.out.print("|");
+
+            for (int column = 0; column < getWidth(); column++)
+            {
+                String value = String.valueOf(getSquare(column, row));
+                if (value.equals("0"))
+                {
+                    value = " # ";
+                } else if (value.length() == 1)
+                {
+                    value = " " + value + " ";
+                } else if (value.length() == 2)
+                {
+                    value = " " + value;
+                }
+                System.out.print(value);
+                System.out.print("|");
+            }
+            System.out.print("\n");
+        }
+        for (int i = 0; i < getWidth(); i++)
+        {
+            System.out.print("+ - ");
+        }
+        System.out.println("+\n");
+    }
+
+
+}
--- Week6/src/Main.java
+++ Week6/src/Main.java
@@ -3,6 +3,11 @@ public class Main
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        Board board = new Board(5, 5, 10);
+        board.printBoard();
+        HillClimber hillClimber = new HillClimber(board);
+        hillClimber.solveBoard(10);
     }
+
+
 }
