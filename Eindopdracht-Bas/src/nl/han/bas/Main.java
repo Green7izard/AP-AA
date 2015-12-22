778c0e43c49d624536d25e4022ea018d6ef7c921
Bas van Summeren <basvanummeren@hotmail.com>
2015-12-22 17:13:59 +0100

    Main with command line for the Anagram finder

--- Eindopdracht-Bas/src/nl/han/bas/Main.java
+++ Eindopdracht-Bas/src/nl/han/bas/Main.java
@@ -1,5 +1,9 @@
 package nl.han.bas;
 
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
 /**
  * Anagrammen zijn woorden die uit dezelfde letters zijn samengesteld.
  * Voorbeeld: ‘een’, ‘nee’, en ‘ene’ zijn anagrammen van elkaar.
@@ -11,8 +15,37 @@ package nl.han.bas;
 public class Main
 {
 
+    private static final int LINELENGTH = 100;
+
     public static void main(String[] args)
     {
-
+        Scanner scan = new Scanner(System.in);
+        AnagramFinder finder = new AnagramFinder();
+        System.out.println("Anagram Finder: Type \"quit\" to shut down. Type anything else to get all possible permutations!");
+        while (true)
+        {
+            String input = scan.nextLine();
+            if (input.equals("quit"))
+            {
+                break;
+            } else
+            {
+                Set<String> anagrams = finder.getAnagrams(input);
+                System.out.println("Anagrams for the words in: \"" + input + "\"");
+                String currentLine = "";
+                for (Iterator<String> it = anagrams.iterator(); it.hasNext(); )
+                {
+                    String current = it.next();
+                    if (currentLine.length() + current.length() > LINELENGTH)
+                    {
+                        System.out.println(currentLine);
+                        currentLine = "";
+                    }
+                    currentLine += current + ", ";
+                }
+                System.out.println(currentLine + "\n");
+            }
+        }
+        System.out.println("End of the program!");
     }
 }
